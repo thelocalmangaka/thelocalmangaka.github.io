d2dcf0eb079fd2606e6f807ff021deb39f7698b8
Tim Nguyen <the.local.mangaka@gmail.com>
2024-08-18 00:07:16 -0700

    message

--- pages/projects/ig_insights/js/helper/login.js
+++ pages/projects/ig_insights/js/helper/login.js
@@ -3,7 +3,7 @@ import {APP_ID} from "../constants/facebook.js";
 export function hideLogin(isLoggedIn) {
     document.getElementById('login').style.display = isLoggedIn ? 'none' : 'block';
     document.getElementById('logout').style.display = isLoggedIn ? 'block' : 'none';
-    document.getElementById('message').innerText = isLoggedIn ? 'block' : 'none';
+    document.getElementById('message').style.display = isLoggedIn ? 'block' : 'none';
 }
 
 export function deleteCookie() {
