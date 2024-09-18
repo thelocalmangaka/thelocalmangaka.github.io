8a944691a07caf27557d0777a6930586b4c79cf6
Tim Nguyen <the.local.mangaka@gmail.com>
2024-09-17 18:48:25 -0700

    permissions

--- pages/projects/ig_insights/js/constants/facebook.js
+++ pages/projects/ig_insights/js/constants/facebook.js
@@ -2,7 +2,7 @@
 import {APP_ID_COOKIE} from "./cookie.js";
 
 const APP_ID = $.cookie(APP_ID_COOKIE);
-export const APP_LOGIN_URL =`https://www.facebook.com/dialog/oauth?scope=instagram_basic,instagram_manage_insights,pages_read_engagement,pages_show_list&client_id=${APP_ID}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${window.location.origin + window.location.pathname}&response_type=token`;
+export const APP_LOGIN_URL =`https://www.facebook.com/dialog/oauth?scope=business_management,ads_management,instagram_basic,instagram_manage_insights,pages_read_engagement,pages_show_list&client_id=${APP_ID}&display=page&extras={"setup":{"channel":"IG_API_ONBOARDING"}}&redirect_uri=${window.location.origin + window.location.pathname}&response_type=token`;
 
 export const GRAPH_API = "https://graph.facebook.com/";
 
